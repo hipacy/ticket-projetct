cda010b8a797b9892dd802a457b99c43503fda62
rocketzki <michal.markieta@gmail.com>
2019-11-20 19:09:41 +0100

    initial commit

---
+++ src/main/java/app/controller/MainController.java
@@ -0,0 +1,19 @@
+package app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+
+    @GetMapping("/")
+    public ModelAndView main(ModelAndView mav) {
+
+
+        return mav;
+    }
+
+
+}
