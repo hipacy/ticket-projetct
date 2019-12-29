78e14920c4ac17edb22bae84ebcfcd91771dae64
vnskln <vnsklndev@gmail.com>
2019-12-29 20:23:47 +0100

    Fix - me cannot into git commits

--- src/main/java/app/repository/MessageRepository.java
+++ src/main/java/app/repository/MessageRepository.java
@@ -1,4 +1,15 @@
 package app.repository;
 
-public class MessageRepository {
+import app.model.Message;
+import app.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+
+    List<Message> findMessageByTicketId(Ticket ticket);
+
 }
