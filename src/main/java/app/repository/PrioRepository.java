3c562727836fd7b4e7fc4beb37b63cb24feee527
vnskln <vnsklndev@gmail.com>
2020-01-03 14:59:15 +0100

    cannot into git v2

--- src/main/java/app/repository/PrioRepository.java
+++ src/main/java/app/repository/PrioRepository.java
@@ -1,4 +1,20 @@
 package app.repository;
 
-public class PrioRepository {
+
+import app.model.Prio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PrioRepository extends JpaRepository<Prio, Integer> {
+
+    @Query(value = "select * from Prio", nativeQuery = true)
+    List<Prio> findAllPrios();
+
+    @Query("select t from Prio t where t.prioId = ?1")
+    Prio findPrioById(Integer id);
+
 }
