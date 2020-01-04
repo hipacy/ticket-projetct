ee5346cbcd0c508cd47d570117642949f97623fa
Bartosz Matejek <bartoszmatejek97@gmail.com>
2020-01-04 15:19:53 +0100

    added LoginController

---
+++ src/main/java/app/controller/LoginController.java
@@ -0,0 +1,18 @@
+package app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+
+    @GetMapping("/login")
+    public ModelAndView login(ModelAndView modelAndView) {
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+
+}
