517ba7e70f96b49b5b6dcce27bae82048d7419d6
Bartosz Matejek <bartoszmatejek97@gmail.com>
2020-01-04 14:50:28 +0100

    added methods findAllTeams and findAllTicketTeams to TeamService

--- src/main/java/app/service/TeamService.java
+++ src/main/java/app/service/TeamService.java
@@ -1,8 +1,11 @@
 package app.service;
 
+import app.model.Team;
 import app.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("teamService")
 public class TeamService {
@@ -14,4 +17,13 @@ public class TeamService {
         this.teamRepository = teamRepository;
 
     }
+
+    public List<Team> findAllTeams() {
+        return teamRepository.findAllTeams();
+    }
+
+    public List<Team> findAllTicketTeams(Integer id) {
+        return teamRepository.findTeamsByTicket(id);
+    }
+
 }
