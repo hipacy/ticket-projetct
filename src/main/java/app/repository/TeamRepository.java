0b11c58732695b8d3337efeb5ab14f3a98dc8ab8
Bartosz Matejek <bartoszmatejek97@gmail.com>
2020-01-04 14:39:00 +0100

    added UserTypeRepository and TeamRepository

---
+++ src/main/java/app/repository/TeamRepository.java
@@ -0,0 +1,9 @@
+package app.repository;
+
+import app.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Integer> {
+}
---
+++ src/main/java/app/repository/UserTypeRepository.java
@@ -0,0 +1,9 @@
+package app.repository;
+
+import app.model.UserType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserTypeRepository extends JpaRepository<UserType, Integer> {
+}
