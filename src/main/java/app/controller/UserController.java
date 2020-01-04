185155edf4a2cbe0a018549f1f46585af7cc42a0
Bartosz Matejek <bartoszmatejek97@gmail.com>
2020-01-04 14:58:40 +0100

    added methods getUser and getUserById to UserController

--- src/main/java/app/controller/UserController.java
+++ src/main/java/app/controller/UserController.java
@@ -2,10 +2,13 @@ package app.controller;
 
 import app.model.names.ModelNames;
 import app.model.names.TemplateNames;
+import app.service.TeamService;
 import app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,6 +18,27 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private TeamService teamService;
+
+    @GetMapping(value = {"/profile"})
+    public ModelAndView getUser(ModelAndView modelAndView) {
+        modelAndView.addObject(ModelNames.USER_MODEL_NAME, userService.findUserByEmail(
+                SecurityContextHolder.getContext().getAuthentication().getName()));
+        modelAndView.addObject(ModelNames.EDIT_MODEL_NAME, false);
+
+        modelAndView.setViewName(TemplateNames.PROFILE_TEMPLATE_NAME);
+        return modelAndView;
+    }
+
+    @GetMapping(value = {"/profile-{id}"})
+    public ModelAndView getUserById(ModelAndView modelAndView, @PathVariable Integer id) {
+        modelAndView.addObject(ModelNames.USER_MODEL_NAME, userService.findUsersByUserId(id));
+        modelAndView.addObject(ModelNames.EDIT_MODEL_NAME, false);
+
+        modelAndView.setViewName(TemplateNames.PROFILE_TEMPLATE_NAME);
+        return modelAndView;
+    }
 
     @GetMapping(value = {"/usersList"})
     public ModelAndView getAllUsers(ModelAndView modelAndView) {
